f9a80a17433c4843a4d1f9ebbdb68ae38c8187d5
Ashifujjman Rafi <ajrafi2722@gmail.com>
2025-08-18 16:41:46 +0900

    research note updated

--- mywork/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-import numpy as np
-import matplotlib as mpl
-
-print(f"Python version: {sys.version}")
-print(f"NumPy version: {np.__version__}")
-# print(f"Matplotlib version: {mpl.__version__}")
